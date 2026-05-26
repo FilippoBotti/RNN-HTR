f6c0044c8a57aa91b1f8b6079f278cf83810bece
FilippoBotti <filippo.botti.itis@gmail.com>
2026-05-26 09:12:52 +0000

    Add layerscale

--- model/longformer.py
+++ model/longformer.py
@@ -1,6 +1,16 @@
 import torch
 import torch.nn as nn
-from timm.models.vision_transformer import Mlp, DropPath, LayerScale
+from timm.models.vision_transformer import Mlp, DropPath
+
+class LayerScale(nn.Module):
+    def __init__(self, dim, init_values=1e-5, inplace=False):
+        super().__init__()
+        self.inplace = inplace
+        self.gamma = nn.Parameter(init_values * torch.ones(dim))
+
+    def forward(self, x):
+        return x.mul_(self.gamma) if self.inplace else x * self.gamma
+
 
 class LongformerLocalAttention(nn.Module):
     def __init__(
